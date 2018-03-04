46de5ef9da6fdd56c741009fca0b031a9606c7f7
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-03-04 14:35:53 -0800

    Create wrong-turn.ino

---
+++ wrong-turn.ino
@@ -0,0 +1,28 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPinG = 5;
+int ledPinB = 6;
+int ledPinR = 9;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int count = 0;
+int sum = 1;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPinG, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(ledPinG, HIGH);
+
+  delay(10);
+  digitalWrite(ledPinG, LOW);
+  delay(10);
+  digitalWrite(ledPinG, HIGH);
+  delay(10);
+  digitalWrite(ledPinG, LOW);
+  delay(1000);
+
+  
+  // stop the program for for <sensorValue> milliseconds:
+}
