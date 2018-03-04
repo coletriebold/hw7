8e54c05de106617a4f0beb2388888c25aafb528a
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-03-04 14:34:39 -0800

    Create urgent.ino

---
+++ urgent.ino
@@ -0,0 +1,24 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPinG = 5;
+int ledPinB = 6;
+int ledPinR = 9;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int count = 0;
+int sum = 1;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPinG, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(ledPinG, HIGH);
+
+  delay(10);
+  digitalWrite(ledPinG, LOW);
+  delay(100);
+
+  
+  // stop the program for for <sensorValue> milliseconds:
+}
